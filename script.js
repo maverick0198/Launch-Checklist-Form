dd86a2687d1bb1299b023582a5ea3754cc7b22e4
Chris Endicott <maverick0198@gmail.com>
2021-04-06 15:59:48 -0500

    Final product. Endicott assignment 5 plus bonus.

--- script.js
+++ script.js
@@ -1,28 +1,81 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   // fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
-   //    console.log(response);
-   // } );
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+
+         let i= Math.floor(Math.random() * json.length);
+         
+         document.getElementById("missionTarget").innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[i].name}</li>
+            <li>Diameter: ${json[i].diameter}</li>
+            <li>Star: ${json[i].star}</li>
+            <li>Distance from Earth: ${json[i].distance}</li>
+            <li>Number of Moons: ${json[i].moons}</li>
+         </ol>
+         <img src="${json[i].image}">
+         `;
+      });
+   });
 
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       //stops the form submission
       event.preventDefault();
-      alert("submit clicked");
+      
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let coPilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-      // alert the current value found in the username input
-      alert("username: " + pilotNameInput.value);
+
+      
       //Adding Alerts -Chris
 
-      //This validates all fields are filled. -chris
+      //This validates all fields are filled... and valid input -chris
       if(pilotNameInput.value.trim() === "" || coPilotNameInput.value.trim() === "" || fuelLevelInput.value.trim() === "" || cargoMassInput.value.trim() === ""){
          alert("You must input all fields before submitting the form!")
+      }else if( isNaN(pilotNameInput.value) === false ) {
+         alert("Invalid Input: Pilot Name can not be a number.")
+      }else if( isNaN(coPilotNameInput.value) === false ) {
+         alert("Invalid Input: Co-pilot Name can not be a number.")
+      }else if( isNaN(fuelLevelInput.value) === true ) {
+         alert("Invalid Input: Fuel level can only be a number.")
+      }else if( isNaN(cargoMassInput.value) === true ) {
+         alert("Invalid Input: Cargo mass can only be a number.")
+      }else {
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} Ready`;
+         document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${coPilotNameInput.value} Ready`;
+         document.getElementById("fuelStatus").innerHTML = "Fuel Level high enough for launch";
+         document.getElementById("cargoStatus").innerHTML = "Cargo Mass low enough for launch";
+
+         document.getElementById('launchStatus').innerHTML = 'Awaiting Information Before Launch';
+         document.getElementById('launchStatus').style.color= "black";
+
+
+         if(fuelLevelInput.value < 10000){
+            document.getElementById("fuelStatus").innerHTML = `There is not enough fuel for the journey. Shuttle will require at least 10,000 liters!`
+            document.getElementById('launchStatus').innerHTML = 'Shuttle not ready for launch!';
+            document.getElementById('launchStatus').style.color= "red";
+         } 
+         if(cargoMassInput.value > 10000){
+            document.getElementById("cargoStatus").innerHTML = "There is too much mass for the shuttle to take off. Cargo must be under 10,000 kilograms!"
+            document.getElementById('launchStatus').innerHTML = 'Shuttle not ready for launch!';
+            document.getElementById('launchStatus').style.color= "red";
+         }
+         if(fuelLevelInput.value >= 10000 && cargoMassInput.value <= 10000){
+            document.getElementById('launchStatus').innerHTML = 'Shuttle is ready for launch';
+            document.getElementById('launchStatus').style.color= "green";
+
+         }
+
+
+         document.getElementById("faultyItems").style.visibility = "visible";
       }
 
-      if(typeOf(pilotNameInput.value)
+
+      
+      
    });
 
 
